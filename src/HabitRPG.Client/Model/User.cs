79f08e4572989ed2fa411a8b7943d97bb9bd0706
Josh Davis <joshdav@microsoft.com>
2017-10-05 23:20:41 -0700

    Updating User model

--- src/HabitRPG.Client/Model/User.cs
+++ src/HabitRPG.Client/Model/User.cs
@@ -6,20 +6,8 @@ namespace HabitRPG.Client.Model
 {
     public class User : Member
     {
-        [JsonProperty("dailys")]
-        public List<Daily> Dailys { get; set; }
-
-        [JsonProperty("habits")]
-        public List<Habit> Habits { get; set; }
-
-        [JsonProperty("rewards")]
-        public List<Reward> Rewards { get; set; }
-
-        [JsonProperty("todos")]
-        public List<Todo> Todos { get; set; }
-
         [JsonProperty("balance")]
-        public double Balance { get; set; }
+        public double Gems { get; set; }
 
         [JsonProperty("tags")]
         public List<Tag> Tags { get; set; }
